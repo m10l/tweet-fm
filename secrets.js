2a4f22e2810ecbb2b74f3be13a4d0de33e8980b2
Mike Mitchell <mikemitchell@Mikes-MacBook-Air.local>
2016-02-14 23:26:00 +0000

    changed case of env vars

--- secrets.js
+++ secrets.js
@@ -1,8 +1,8 @@
 module.exports = {
     twitter: {
-        consumer_key: process.env.twitter_consumer_key || 'YOUR_CONSUMER_KEY',
-        consumer_secret: process.env.twitter_consumer_secret || 'YOUR_CONSUMER_SECRET',
-        access_token: process.env.twitter_access_token || 'YOUR_ACCESS_TOKEN',
-        access_token_secret: process.env.twitter_access_token_secret || 'YOUR_ACCESS_TOKEN_SECRET'
+        consumer_key: process.env.TWITTER_CONSUMER_KEY || 'YOUR_CONSUMER_KEY',
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET || 'YOUR_CONSUMER_SECRET',
+        access_token: process.env.TWITTER_ACCESS_TOKEN || 'YOUR_ACCESS_TOKEN',
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET || 'YOUR_ACCESS_TOKEN_SECRET'
     }
 };
